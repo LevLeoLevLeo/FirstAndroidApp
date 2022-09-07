6078a0c0161fa3beb2e47d1aa0eee078b16eecba
User <user@kp11.ru>
2022-09-07 16:10:51 +0300

    Check password fields empty/notempty, work with alertdialog in progress.

--- app/src/main/java/com/example/firstapp/RegistrationFragment.kt
+++ app/src/main/java/com/example/firstapp/RegistrationFragment.kt
@@ -1,6 +1,8 @@
 package com.example.firstapp
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.opengl.Visibility
 import android.os.Bundle
 import android.text.InputType
@@ -10,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import com.example.firstapp.databinding.RegistrationFragmentBinding
@@ -23,17 +26,22 @@ class RegistrationFragment : Fragment(R.layout.registration_fragment) {
                               savedInstanceState: Bundle?): View
     {
         _binding = RegistrationFragmentBinding.inflate(inflater, container, false)
+
         binding.btnRegistration.setOnClickListener { regAccount() }
         binding.swtchShowPass.setOnClickListener { showPass() }
-        binding.txtEditNewPassword.doOnTextChanged { _, _, _, _ ->  checkPass()}
-        binding.txtEditRepeatNewPassword.doOnTextChanged { _, _, _, _ -> checkPass() }
+        binding.txtEditNewPassword.addTextChangedListener { checkPass()}
+        binding.txtEditRepeatNewPassword.addTextChangedListener { checkPass() }
 
         return binding.root
     }
     override fun onDestroyView() {
+        closePage()
         super.onDestroyView()
         _binding = null
     }
+
+    //Functions
+
     private fun regAccount() {
         if (binding.txtEditNewLogin.text.isNotEmpty() && binding.txtEditNewPassword.text.isNotEmpty() &&
                 binding.txtEditRepeatNewPassword.text.isNotEmpty())
@@ -54,13 +62,32 @@ class RegistrationFragment : Fragment(R.layout.registration_fragment) {
         binding.txtEditNewPassword.setSelection(binding.txtEditNewPassword.length())
     }
     private fun checkPass(){
-        if (binding.txtEditNewPassword.text.toString() == binding.txtEditRepeatNewPassword.text.toString()){
-            binding.txtPassNotEqual.isVisible
+        if (binding.txtEditNewPassword.text.isEmpty() || binding.txtEditRepeatNewPassword.text.isEmpty())
+        {
+            binding.txtPassNotEqual.isGone = true
             binding.btnRegistration.isEnabled = false
         }
         else {
-            binding.txtPassNotEqual.isGone
-            binding.btnRegistration.isEnabled
+                if (binding.txtEditNewPassword.text.toString() != binding.txtEditRepeatNewPassword.text.toString()){
+                    binding.txtPassNotEqual.isVisible = true
+                    binding.btnRegistration.isEnabled = false
+                }
+                else
+                {
+                    binding.txtPassNotEqual.isGone = true
+                    binding.btnRegistration.isEnabled = true
+                }
+            }
         }
+    private fun closePage(){
+
+//        if (binding.txtEditNewLogin.text.isNotEmpty() || binding.txtEditNewPassword.text.isNotEmpty() ||
+//            binding.txtEditRepeatNewPassword.text.isNotEmpty()){
+//            val builder = AlertDialog.Builder(layoutInflater.context)
+//            builder.setTitle("Закрытие страницы")
+//            builder.setMessage("При закрытии страницы все введенные данные будут удалены. Продолжить?")
+//            builder.show()
+//        }
+        Toast.makeText(this.activity, "Проверка работы функций с DestroyView.", Toast.LENGTH_SHORT).show()
     }
-}
+    }
